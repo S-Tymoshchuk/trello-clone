bf797f1f31efcfc51930c633694748692b06e98d
Sergey <lobalmax@gmail.com>
2019-12-20 18:17:06 +0200

    Updates

---
+++ precache-manifest.040d52070e375b3dd79505d180fd2b11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e9f9d1750dee73336f5a9006880235",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "6f563e7018f0c6b0d292",
+    "url": "/trello-clone/static/js/2.5200030e.chunk.js"
+  },
+  {
+    "revision": "5ba8ceb64eea848335a496f62ab9063a",
+    "url": "/trello-clone/static/js/2.5200030e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d38b26a947c227ca502e",
+    "url": "/trello-clone/static/js/main.c8212d23.chunk.js"
+  },
+  {
+    "revision": "751794906de79d4751d4",
+    "url": "/trello-clone/static/js/runtime-main.e2fc9f5b.js"
+  }
+]);
